fe3822368125c46339aa18e33a850a2a94d8907f
Abdellah-Nsila <dawndawn074@gmail.com>
2025-05-23 20:31:11 +0100

    Working in buitins, setup env for minishell

--- srcs/setup.c
+++ srcs/setup.c
@@ -6,7 +6,7 @@
 /*   By: abnsila <abnsila@student.1337.ma>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/19 11:49:54 by abnsila           #+#    #+#             */
-/*   Updated: 2025/05/23 20:00:04 by abnsila          ###   ########.fr       */
+/*   Updated: 2025/05/23 20:31:07 by abnsila          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,42 @@
 //TODO: The anv of your shell must be setuped correctly
 //TODO: Create those utils: add_var, get_var, pop_var, 
 //TODO: also:
+
+t_bool	var_exist(char *key)
+{
+	int		i;
+	int		len;
+
+	i = 0;
+	len = ft_strlen(key);
+	while (sh.env[i])
+	{
+		if (ft_strncmp(sh.env[i], key, len) == 0)
+		{
+			return (true);
+		}
+		i++;
+	}
+	return (false);
+}
+
+void	unset_var(char *key)
+{
+	int		i;
+	int		len;
+	char	new_env = ft_calloc(1, sizeof(char *));
+
+	i = 0;
+	len = ft_strlen(key);
+	while (sh.env[i])
+	{
+		if (ft_strncmp(sh.env[i], key, len) == 0)
+		{
+			return (true);
+		}
+		i++;
+	}
+}
 
 char	*get_value(char *key)
 {
@@ -36,9 +72,11 @@ char	*get_value(char *key)
 	}
 }
 
-void	set_var(char *key, char *value)
+void	add_var(char *key, char *value)
 {
-	
+	if (var_exist(key))
+		unset_var(key)
+	// You nust unset then add
 }
 
 void	setup_env(char **env)
