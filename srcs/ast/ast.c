887cd35181e1dd83085f746546a1a49f68584cdc
Abdellah-Nsila <dawndawn074@gmail.com>
2025-05-23 18:05:49 +0100

    Change AST utils for new struct (t_ast *children)

--- srcs/ast/ast.c
+++ srcs/ast/ast.c
@@ -6,102 +6,95 @@
 /*   By: abnsila <abnsila@student.1337.ma>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/24 10:13:31 by abnsila           #+#    #+#             */
-/*   Updated: 2025/05/22 18:24:48 by abnsila          ###   ########.fr       */
+/*   Updated: 2025/05/23 17:54:35 by abnsila          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-// Create a new AST node: initializes all fields and sets up self-linked next/prev
-t_ast *ft_new_ast_node(t_gram type)
+// Create a new AST node with no children and cleared union
+t_ast	*ast_new_node(t_gram type)
 {
-	t_ast *node = ft_calloc(1, sizeof(t_ast));
-	if (!node)
-		return NULL;
-	node->type = type;
-	node->left = NULL;
-	node->right = NULL;
-	// initialize union
-	node->data.args = NULL;
-	node->data.redir.file = NULL;
-	node->data.redir.type = UNKNOWN;
-	node->data.redir.limiter = NULL;
-	node->data.redir.expanded = false;
-	return node;
+    t_ast *node = ft_calloc(1, sizeof *node);
+    if (!node) return NULL;
+    node->type     = type;
+    node->children = ft_calloc(1, sizeof(t_ast *)); // just the NULL terminator
+    if (!node->children)
+    {
+        free(node);
+        return NULL;
+    }
+    // initialize union fields
+    node->data.args           = NULL;
+    node->data.redir.type     = UNKNOWN;
+    node->data.redir.file     = NULL;
+    node->data.redir.limiter  = NULL;
+    node->data.redir.expanded = false;
+    return node;
 }
 
-// Helper: build a NULL-terminated args array from strings
-char	**ft_create_args(int count, ...)
+// Append `child` to `parent->children[]`
+int	ast_add_child(t_ast *parent, t_ast *child)
 {
-	if (count < 0) {
-		errno = EINVAL;
-		return NULL;
-	}
+    int n = 0;
+    while (parent->children[n]) n++;
 
-	// Allocate array of (count + 1) pointers
-	char **args = ft_calloc((count + 1), sizeof(char *));
-	if (!args) {
-		return NULL;  // malloc sets errno on failure
-	}
-
-	va_list ap;
-	va_start(ap, count);
-	for (int i = 0; i < count; i++) {
-		const char *s = va_arg(ap, const char *);
-		if (s) {
-			args[i] = strdup(s);
-			if (!args[i]) {
-				// strdup failed: clean up and report
-				for (int j = 0; j < i; j++)
-					free(args[j]);
-				free(args);
-				va_end(ap);
-				return NULL;
-			}
-		} else {
-			args[i] = NULL;
-		}
-	}
-	va_end(ap);
-
-	args[count] = NULL;
-	return args;
+    // resize to hold one more + NULL
+    t_ast **tmp = realloc(parent->children,
+                         sizeof(t_ast *) * (n + 2));
+    if (!tmp) return -1;
+    parent->children = tmp;
+    parent->children[n]     = child;
+    parent->children[n+1]   = NULL;
+    return 0;
 }
 
-// t_ast *ft_new_ast_node(t_gram type)
-// {
-//     t_ast *node = malloc(sizeof(t_ast));
-//     if (!node) return NULL;
-//     node->type = type;
-//     node->data.args = NULL;
-//     node->data.redir.file = NULL;
-//     node->data.redir.append = 0;
-//     node->left = NULL;
-//     node->right = NULL;
-//     return node;
-// }
+char	**ast_create_args(int count, ...)
+{
+    if (count < 0) return NULL;
+    char **args = calloc(count + 1, sizeof *args);
+    if (!args) return NULL;
 
-// char	**ft_create_args(t_token *tokens, int count)
-// {
-//     char **args = malloc(sizeof(char *) * (count + 1));
-//     for (int i = 0; i < count; i++) {
-//         args[i] = strdup(tokens->value);
-//         tokens = tokens->next;
-//     }
-//     args[count] = NULL;
-//     return args;
-// }
+    va_list ap;
+    va_start(ap, count);
+    for (int i = 0; i < count; i++)
+    {
+        const char *s = va_arg(ap, const char *);
+        args[i] = s ? strdup(s) : NULL;
+        if (s && !args[i])
+        {
+            // cleanup on failure
+            while (i-- > 0) free(args[i]);
+            free(args);
+            va_end(ap);
+            return NULL;
+        }
+    }
+    va_end(ap);
+    args[count] = NULL;
+    return args;
+}
 
-// void ft_clear_ast(t_ast *a)
-// {
-//     if (!a) return;
-//     ft_clear_ast(a->left);
-//     ft_clear_ast(a->right);
-//     if (a->type == GRAM_SIMPLE_COMMAND && a->data.args) {
-//         for (char **p = a->data.args; *p; ++p) free(*p);
-//         free(a->data.args);
-//     }
-//     if (a->type == GRAM_IO_REDIRECT && a->data.redir.file)
-//         free(a->data.redir.file);
-//     free(a);
-// }
+void ast_destroy(t_ast *node)
+{
+    if (!node) return;
+
+    // free union
+    if (node->type == GRAM_SIMPLE_COMMAND && node->data.args)
+        clear_arr(node->data.args);
+    else if (node->type == GRAM_IO_REDIRECT && node->data.redir.file)
+    {
+        if (node->type == GRAM_HEREDOC)
+            unlink(node->data.redir.file);
+        free(node->data.redir.file);
+        if (node->data.redir.limiter)
+            free(node->data.redir.limiter);
+    }
+
+    // destroy children
+    for (int i = 0; node->children && node->children[i]; i++)
+        ast_destroy(node->children[i]);
+
+    free(node->children);
+    free(node);
+}
