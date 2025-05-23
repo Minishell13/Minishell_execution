10db6977fe44bfb254236660eee90b3186736ba8
Abdellah-Nsila <dawndawn074@gmail.com>
2025-05-23 20:00:28 +0100

    Working in buitins, setup env for minishell

--- srcs/setup.c
+++ srcs/setup.c
@@ -6,11 +6,40 @@
 /*   By: abnsila <abnsila@student.1337.ma>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/19 11:49:54 by abnsila           #+#    #+#             */
-/*   Updated: 2025/05/19 12:02:49 by abnsila          ###   ########.fr       */
+/*   Updated: 2025/05/23 20:00:04 by abnsila          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+
+//TODO: The anv of your shell must be setuped correctly
+//TODO: Create those utils: add_var, get_var, pop_var, 
+//TODO: also:
+
+char	*get_value(char *key)
+{
+	int		i;
+	int		len;
+	char	*value;
+
+	i = 0;
+	len = ft_strlen(key);
+	while (sh.env[i])
+	{
+		if (ft_strncmp(sh.env[i], key, len) == 0)
+		{
+			value = sh.env[i] + len;
+			value = ft_strdup(value + 1);
+			return (value);
+		}
+		i++;
+	}
+}
+
+void	set_var(char *key, char *value)
+{
+	
+}
 
 void	setup_env(char **env)
 {
