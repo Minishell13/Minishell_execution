29247152b29ccd3d5070634d35597e081696c902
Abdellah-Nsila <dawndawn074@gmail.com>
2025-05-24 17:45:57 +0100

    Test Builtins with executor

---
+++ srcs/execution/builtins/builtins.c
@@ -0,0 +1,78 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   builtins.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: abnsila <abnsila@student.1337.ma>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/24 16:05:55 by abnsila           #+#    #+#             */
+/*   Updated: 2025/05/24 17:16:33 by abnsila          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+t_bool	is_builtins(t_ast *node)
+{
+	char	*cmd;
+
+	cmd = node->data.args[0];
+	if (ft_strlcmp("cd", cmd))
+		return (true);
+	else if (ft_strlcmp("echo", cmd))
+		return (true);
+	else if (ft_strlcmp("env", cmd))
+		return (true);
+	else if (ft_strlcmp("exit", cmd))
+		return (true);
+	else if (ft_strlcmp("export", cmd))
+		return (true);
+	else if (ft_strlcmp("pwd", cmd))
+		return (true);
+	else if (ft_strlcmp("unset", cmd))
+		return (true);
+	return (false);
+}
+
+t_error	exec_builtins(t_ast *root, t_ast *node)
+{
+	char	*cmd;
+
+	cmd = node->data.args[0];
+	if (ft_strlcmp("cd", cmd))
+	{
+		expand_cmd_node(node, process_mode_1);	
+		return (exec_cd(node));
+	}
+	else if (ft_strlcmp("echo", cmd))
+	{
+		expand_cmd_node(node, process_mode_1);	
+		return (exec_echo(node));
+	}
+	else if (ft_strlcmp("env", cmd))
+	{
+		expand_cmd_node(node, process_mode_1);	
+		return (exec_env(node));
+	}
+	else if (ft_strlcmp("exit", cmd))
+	{
+		expand_cmd_node(node, process_mode_1);	
+		return (exec_exit(root));
+	}
+	else if (ft_strlcmp("export", cmd))
+	{
+		expand_cmd_node(node, process_mode_2);	
+		return (exec_export(node));
+	}
+	else if (ft_strlcmp("pwd", cmd))
+	{
+		expand_cmd_node(node, process_mode_1);	
+		return (exec_pwd(node));
+	}
+	else if (ft_strlcmp("unset", cmd))
+	{
+		expand_cmd_node(node, process_mode_1);	
+		return (exec_unset(node));
+	}
+	return (SUCCESS);
+}
--- srcs/execution/executor.c
+++ srcs/execution/executor.c
@@ -6,7 +6,7 @@
 /*   By: abnsila <abnsila@student.1337.ma>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/23 17:45:09 by abnsila           #+#    #+#             */
-/*   Updated: 2025/05/24 10:38:15 by abnsila          ###   ########.fr       */
+/*   Updated: 2025/05/24 17:15:49 by abnsila          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -66,8 +66,13 @@ t_error	ft_executor(t_ast *root, t_ast *node, char **envp)
 	case GRAM_SIMPLE_COMMAND:
 	{
 		// TODO: If buitins == export expand just $VAR not * Wildcard
-		expand_cmd_node(node);
-		return ft_execute_simple_cmd(root, node, envp);
+		if (is_builtins(node))
+			return (exec_builtins(root, node));
+		else
+		{
+			expand_cmd_node(node, process_mode_1);
+			return (ft_execute_simple_cmd(root, node, envp));
+		}
 	}
 
 	case GRAM_SUBSHELL:
