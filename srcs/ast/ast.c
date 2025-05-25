fa5b3bed01059aa5fc9a9a91c955686b26f53b56
Abdellah-Nsila <dawndawn074@gmail.com>
2025-05-25 18:57:40 +0100

    Update AST utils

--- srcs/ast/ast.c
+++ srcs/ast/ast.c
@@ -6,95 +6,114 @@
 /*   By: abnsila <abnsila@student.1337.ma>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/24 10:13:31 by abnsila           #+#    #+#             */
-/*   Updated: 2025/05/23 17:54:35 by abnsila          ###   ########.fr       */
+/*   Updated: 2025/05/25 11:03:09 by abnsila          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-// Create a new AST node with no children and cleared union
-t_ast	*ast_new_node(t_gram type)
+t_ast *ast_new_node(t_gram type)
 {
-    t_ast *node = ft_calloc(1, sizeof *node);
-    if (!node) return NULL;
-    node->type     = type;
-    node->children = ft_calloc(1, sizeof(t_ast *)); // just the NULL terminator
-    if (!node->children)
-    {
-        free(node);
-        return NULL;
-    }
-    // initialize union fields
-    node->data.args           = NULL;
-    node->data.redir.type     = UNKNOWN;
-    node->data.redir.file     = NULL;
-    node->data.redir.limiter  = NULL;
-    node->data.redir.expanded = false;
-    return node;
+	t_ast *n = ft_calloc(1, sizeof *n);
+	if (!n) return NULL;
+
+	n->type    = type;
+	n->child   = NULL;
+	n->sibling = NULL;
+
+	// init union
+	n->data.args           = NULL;
+	n->data.redir.type     = UNKNOWN;
+	n->data.redir.file     = NULL;
+	n->data.redir.limiter  = NULL;
+	n->data.redir.expanded = false;
+
+	return n;
 }
 
-// Append `child` to `parent->children[]`
-int	ast_add_child(t_ast *parent, t_ast *child)
+int ast_add_child(t_ast *p, t_ast *c)
 {
-    int n = 0;
-    while (parent->children[n]) n++;
-
-    // resize to hold one more + NULL
-    t_ast **tmp = realloc(parent->children,
-                         sizeof(t_ast *) * (n + 2));
-    if (!tmp) return -1;
-    parent->children = tmp;
-    parent->children[n]     = child;
-    parent->children[n+1]   = NULL;
-    return 0;
+	if (!p || !c) return -1;
+	if (!p->child) {
+		p->child = c;
+	} 
+	else {
+		t_ast *it = p->child;
+		while (it->sibling)
+			it = it->sibling;
+		it->sibling = c;
+	}
+	return 0;
 }
 
-char	**ast_create_args(int count, ...)
+// Returns an array of words split on any isspace(). Caller must free.
+char	**ast_create_args(const char *s)
 {
-    if (count < 0) return NULL;
-    char **args = calloc(count + 1, sizeof *args);
-    if (!args) return NULL;
+	if (!s) return NULL;
 
-    va_list ap;
-    va_start(ap, count);
-    for (int i = 0; i < count; i++)
-    {
-        const char *s = va_arg(ap, const char *);
-        args[i] = s ? strdup(s) : NULL;
-        if (s && !args[i])
-        {
-            // cleanup on failure
-            while (i-- > 0) free(args[i]);
-            free(args);
-            va_end(ap);
-            return NULL;
-        }
-    }
-    va_end(ap);
-    args[count] = NULL;
-    return args;
+	// 1) Count how many words
+	int count = 0;
+	const char *p = s;
+	while (*p) {
+		// skip whitespace
+		while (*p && isspace((unsigned char)*p)) p++;
+		if (!*p) break;
+		// at start of word
+		count++;
+		// skip non-whitespace
+		while (*p && !isspace((unsigned char)*p)) p++;
+	}
+
+	// 2) Allocate array
+	char **args = calloc(count + 1, sizeof *args);
+	if (!args) return NULL;
+
+	// 3) Extract each word
+	p = s;
+	for (int i = 0; i < count; i++) {
+		// skip whitespace
+		while (*p && isspace((unsigned char)*p)) p++;
+		// start of word
+		const char *start = p;
+		while (*p && !isspace((unsigned char)*p)) p++;
+		size_t len = p - start;
+		args[i] = strndup(start, len);
+		if (!args[i]) {
+			// cleanup on failure
+			for (int j = 0; j < i; j++) free(args[j]);
+			free(args);
+			return NULL;
+		}
+	}
+	args[count] = NULL;
+	return args;
 }
 
-void ast_destroy(t_ast *node)
+void    ast_destroy(t_ast *n)
 {
-    if (!node) return;
+	if (!n) return;
 
-    // free union
-    if (node->type == GRAM_SIMPLE_COMMAND && node->data.args)
-        clear_arr(node->data.args);
-    else if (node->type == GRAM_IO_REDIRECT && node->data.redir.file)
-    {
-        if (node->type == GRAM_HEREDOC)
-            unlink(node->data.redir.file);
-        free(node->data.redir.file);
-        if (node->data.redir.limiter)
-            free(node->data.redir.limiter);
-    }
+	// destroy entire child subtree
+	ast_destroy(n->child);
 
-    // destroy children
-    for (int i = 0; node->children && node->children[i]; i++)
-        ast_destroy(node->children[i]);
+	// free this node’s payload
+	if (n->type == GRAM_SIMPLE_COMMAND && n->data.args)
+		clear_arr(n->data.args);
+	else if ((n->type == GRAM_IO_REDIRECT
+		   || n->type == GRAM_REDIR_IN
+		   || n->type == GRAM_REDIR_OUT
+		   || n->type == GRAM_REDIR_APPEND
+		   || n->type == GRAM_HEREDOC)
+		  && n->data.redir.file)
+	{
+		if (n->type == GRAM_HEREDOC)
+			unlink(n->data.redir.file);
+		free(n->data.redir.file);
+		if (n->data.redir.limiter)
+			free(n->data.redir.limiter);
+	}
 
-    free(node->children);
-    free(node);
+	// keep going with siblings
+	ast_destroy(n->sibling);
+	free(n);
 }
--- srcs/ast/ast_examples.c
+++ srcs/ast/ast_examples.c
@@ -6,170 +6,177 @@
 /*   By: abnsila <abnsila@student.1337.ma>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/30 14:49:24 by abnsila           #+#    #+#             */
-/*   Updated: 2025/05/23 19:31:54 by abnsila          ###   ########.fr       */
+/*   Updated: 2025/05/25 18:53:13 by abnsila          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-//? cat << A > "''\"$f1\"''"
-t_ast *ft_get_ast0(void)
+// cat << A > "''\"$f1\"''"
+t_ast	*ft_get_ast0(void)
 {
-    // 1) Simple command: cat
-    t_ast *cmd = ast_new_node(GRAM_SIMPLE_COMMAND);
-    cmd->data.args = ast_create_args(1, "cat");
+	t_ast *cmd = ast_new_node(GRAM_SIMPLE_COMMAND);
+	cmd->data.args = ast_create_args("cat");
 
-    // 2) Redirect out to literal "''\"$f1\"''"
-    t_ast *redir_out = ast_new_node(GRAM_IO_REDIRECT);
-    redir_out->data.redir.type = GRAM_REDIR_OUT;
-    redir_out->data.redir.file = strdup("''\"$f1\"''");
-    ast_add_child(redir_out, cmd);
+	t_ast *redir_out = ast_new_node(GRAM_IO_REDIRECT);
+	redir_out->data.redir.type = GRAM_REDIR_OUT;
+	redir_out->data.redir.file = strdup("''\"$f1\"''");
+	ast_add_child(redir_out, cmd);
 
-    // 3) Here‐doc <<A on top of that
-    t_ast *heredoc = ast_new_node(GRAM_IO_REDIRECT);
-    ft_generate_tmpfile(&heredoc->data.redir);
-    heredoc->data.redir.type    = GRAM_HEREDOC;
-    heredoc->data.redir.limiter = strdup("\"\"$a");
-    ast_add_child(heredoc, redir_out);
+	t_ast *heredoc = ast_new_node(GRAM_IO_REDIRECT);
+	generate_tmpfile(&heredoc->data.redir);
+	heredoc->data.redir.type = GRAM_HEREDOC;
+	heredoc->data.redir.limiter = strdup("\"\"$a");
+	ast_add_child(heredoc, redir_out);
 
-    // 4) Wrap in COMPLETE_COMMAND
-    t_ast *root = ast_new_node(GRAM_COMPLETE_COMMAND);
-    ast_add_child(root, heredoc);
-    return root;
+	t_ast *root = ast_new_node(GRAM_COMPLETE_COMMAND);
+	ast_add_child(root, heredoc);
+	return root;
 }
 
-//? echo X && echo Y && echo Z && echo W
-t_ast *ft_get_ast1(void)
+// echo X && echo Y && echo Z && echo W
+t_ast	*ft_get_ast1(void)
 {
-    t_ast *root = ast_new_node(GRAM_COMPLETE_COMMAND);
-    
-    // Build each echo… as a simple command node
-    t_ast *c1 = ast_new_node(GRAM_SIMPLE_COMMAND);
-    c1->data.args = ast_create_args(2, "echo", "'\"$PATH\"'");
-    t_ast *c2 = ast_new_node(GRAM_SIMPLE_COMMAND);
-    c2->data.args = ast_create_args(2, "echo", "\"\"$SHELL\"\"");
-    t_ast *c3 = ast_new_node(GRAM_SIMPLE_COMMAND);
-    c3->data.args = ast_create_args(2, "echo", "\"\"$PWD\"\"");
-    t_ast *c4 = ast_new_node(GRAM_SIMPLE_COMMAND);
-    c4->data.args = ast_create_args(2, "echo", "'\"$CC\"'");
-    
-    // Chain them all under one big AND node
-    t_ast *all_and = ast_new_node(GRAM_OPERATOR_AND);
-    ast_add_child(all_and, c1);
-    ast_add_child(all_and, c2);
-    ast_add_child(all_and, c3);
-    ast_add_child(all_and, c4);
-    
-    ast_add_child(root, all_and);
-    return root;
+	t_ast *root = ast_new_node(GRAM_COMPLETE_COMMAND);
+
+	t_ast *c1 = ast_new_node(GRAM_SIMPLE_COMMAND);
+	c1->data.args = ast_create_args("echo '\"$PATH\"'");
+
+	t_ast *c2 = ast_new_node(GRAM_SIMPLE_COMMAND);
+	c2->data.args = ast_create_args("echo \"\"$SHELL\"\"");
+
+	t_ast *c3 = ast_new_node(GRAM_SIMPLE_COMMAND);
+	c3->data.args = ast_create_args("echo \"\"$PWD\"\"");
+
+	t_ast *c4 = ast_new_node(GRAM_SIMPLE_COMMAND);
+	c4->data.args = ast_create_args("echo '\"$CC\"'");
+
+	
+	t_ast *and1 = ast_new_node(GRAM_OPERATOR_AND);
+	ast_add_child(and1, c1);
+	ast_add_child(and1, c2);
+	
+	t_ast *and2 = ast_new_node(GRAM_OPERATOR_AND);
+	ast_add_child(and2, and1);
+	ast_add_child(and2, c3);
+	
+	
+	t_ast *and3 = ast_new_node(GRAM_OPERATOR_AND);
+	ast_add_child(and3, and2);
+	ast_add_child(and3, c4);
+
+
+	ast_add_child(root, and3);
+	return root;
 }
 
-//? cat | ls > out
-t_ast *ft_get_ast2(void)
+// cat | ls > out
+t_ast	*ft_get_ast2(void)
 {
-    t_ast *root = ast_new_node(GRAM_COMPLETE_COMMAND);
-    
-    t_ast *cat = ast_new_node(GRAM_SIMPLE_COMMAND);
-    cat->data.args = ast_create_args(1, "cat");
-    
-    t_ast *ls = ast_new_node(GRAM_SIMPLE_COMMAND);
-    ls->data.args = ast_create_args(1, "ls");
-    t_ast *redir = ast_new_node(GRAM_REDIR_OUT);
-    redir->data.redir.type = GRAM_REDIR_OUT;
-    redir->data.redir.file = strdup("./temp/out");
-    ast_add_child(redir, ls);
-    
-    t_ast *pipe = ast_new_node(GRAM_PIPELINE);
-    ast_add_child(pipe, cat);
-    ast_add_child(pipe, redir);
-    
-    ast_add_child(root, pipe);
-    return root;
+	t_ast *root = ast_new_node(GRAM_COMPLETE_COMMAND);
+
+	t_ast *redir = ast_new_node(GRAM_IO_REDIRECT);
+	redir->data.redir.type = GRAM_REDIR_OUT;
+	redir->data.redir.file = strdup("./temp/out");
+	
+	t_ast *ls = ast_new_node(GRAM_SIMPLE_COMMAND);
+	ls->data.args = ast_create_args("ls");
+	ast_add_child(ls, redir);
+	
+	t_ast *cat = ast_new_node(GRAM_SIMPLE_COMMAND);
+	cat->data.args = ast_create_args("cat");
+
+	t_ast *pipe = ast_new_node(GRAM_PIPELINE);
+	ast_add_child(pipe, cat);
+	ast_add_child(pipe, ls);
+
+	ast_add_child(root, pipe);
+	return root;
 }
 
-//? echo 1 && echo 2 && echo 3 && echo 4
-t_ast *ft_get_ast3(void)
+// echo 1 && echo 2 && echo 3 && echo 4
+t_ast	*ft_get_ast3(void)
 {
-    t_ast *root = ast_new_node(GRAM_COMPLETE_COMMAND);
+	t_ast *root = ast_new_node(GRAM_COMPLETE_COMMAND);
 
-    // Build each echo… as a simple command node
-    t_ast *c1 = ast_new_node(GRAM_SIMPLE_COMMAND);
-    c1->data.args = ast_create_args(2, "echo", "1");
-    t_ast *c2 = ast_new_node(GRAM_SIMPLE_COMMAND);
-    c2->data.args = ast_create_args(2, "echo", "2");
-    t_ast *c3 = ast_new_node(GRAM_SIMPLE_COMMAND);
-    c3->data.args = ast_create_args(2, "echo", "3");
-    t_ast *c4 = ast_new_node(GRAM_SIMPLE_COMMAND);
-    c4->data.args = ast_create_args(2, "echo", "4");
+	t_ast *c1 = ast_new_node(GRAM_SIMPLE_COMMAND);
+	c1->data.args = ast_create_args("echo 1");
 
-    // Chain them all under one big AND node
-    t_ast *and_3 = ast_new_node(GRAM_OPERATOR_AND);
-    ast_add_child(and_3, c1);
-    ast_add_child(and_3, c2);
-    
-    
-    t_ast *and_2 = ast_new_node(GRAM_OPERATOR_AND);
-    ast_add_child(and_2, and_3);
-    ast_add_child(and_2, c3);
-    
-    t_ast *and_1 = ast_new_node(GRAM_OPERATOR_AND);
-    ast_add_child(and_1, and_2);
-    ast_add_child(and_1, c4);
+	t_ast *c2 = ast_new_node(GRAM_SIMPLE_COMMAND);
+	c2->data.args = ast_create_args("echo 2");
 
-    ast_add_child(root, and_1);
-    return root;
+	t_ast *c3 = ast_new_node(GRAM_SIMPLE_COMMAND);
+	c3->data.args = ast_create_args("echo 3");
+
+	t_ast *c4 = ast_new_node(GRAM_SIMPLE_COMMAND);
+	c4->data.args = ast_create_args("echo 4");
+
+	t_ast *and_3 = ast_new_node(GRAM_OPERATOR_AND);
+	ast_add_child(and_3, c1);
+	ast_add_child(and_3, c2);
+
+	t_ast *and_2 = ast_new_node(GRAM_OPERATOR_AND);
+	ast_add_child(and_2, and_3);
+	ast_add_child(and_2, c3);
+
+	t_ast *and_1 = ast_new_node(GRAM_OPERATOR_AND);
+	ast_add_child(and_1, and_2);
+	ast_add_child(and_1, c4);
+
+	ast_add_child(root, and_1);
+	return root;
 }
 
-//? ls | grep "M" | wc -l && echo OK || echo "ERROR"
-t_ast *ft_get_ast4(void)
+// ls | grep "M" | wc -l && echo OK || echo "ERROR"
+t_ast	*ft_get_ast4(void)
 {
-    t_ast *root = ast_new_node(GRAM_COMPLETE_COMMAND);
-    
-    // cmd | cmd
-    t_ast *c1 = ast_new_node(GRAM_SIMPLE_COMMAND);
-    c1->data.args = ast_create_args(2, "ls");
-    t_ast *c2 = ast_new_node(GRAM_SIMPLE_COMMAND);
-    c2->data.args = ast_create_args(2, "grep", "M");
-    t_ast *p1 = ast_new_node(GRAM_PIPELINE);
-    ast_add_child(p1, c1);
-    ast_add_child(p1, c2);
-    
-    // pipe1 | cmd
-    t_ast *c3 = ast_new_node(GRAM_SIMPLE_COMMAND);
-    c3->data.args = ast_create_args(2, "wc", "-l");
-    t_ast *p2 = ast_new_node(GRAM_PIPELINE);
-    ast_add_child(p2, p1);
-    ast_add_child(p2, c3);
-
-    // pipe2 && cmd
-    t_ast *c4 = ast_new_node(GRAM_SIMPLE_COMMAND);
-    c4->data.args = ast_create_args(2, "echo", "OK");
-    t_ast *and_2 = ast_new_node(GRAM_OPERATOR_AND);
-    ast_add_child(and_2, p2);
-    ast_add_child(and_2, c4);
-    
-
-    t_ast *c5 = ast_new_node(GRAM_SIMPLE_COMMAND);
-    c5->data.args = ast_create_args(2, "echo", "ERROR");
-    t_ast *or = ast_new_node(GRAM_OPERATOR_OR);
-    ast_add_child(or, and_2);
-    ast_add_child(or, c5);
-
-    ast_add_child(root, or);
-    return root;
+	t_ast *c1 = ast_new_node(GRAM_SIMPLE_COMMAND);
+	c1->data.args = ast_create_args("ls");
+	
+	t_ast *c2 = ast_new_node(GRAM_SIMPLE_COMMAND);
+	c2->data.args = ast_create_args("grep M");
+	
+	t_ast *p1 = ast_new_node(GRAM_PIPELINE);
+	ast_add_child(p1, c1);
+	ast_add_child(p1, c2);
+	
+	t_ast *c3 = ast_new_node(GRAM_SIMPLE_COMMAND);
+	c3->data.args = ast_create_args("wc -l");
+	
+	t_ast *p2 = ast_new_node(GRAM_PIPELINE);
+	ast_add_child(p2, p1);
+	ast_add_child(p2, c3);
+	
+	// t_ast *c4 = ast_new_node(GRAM_SIMPLE_COMMAND);
+	// c4->data.args = ast_create_args("echo OK");
+	
+	// t_ast *and_2 = ast_new_node(GRAM_OPERATOR_AND);
+	// ast_add_child(and_2, p2);
+	// ast_add_child(and_2, c4);
+	
+	// t_ast *c5 = ast_new_node(GRAM_SIMPLE_COMMAND);
+	// c5->data.args = ast_create_args("echo ERROR");
+	
+	// t_ast *or = ast_new_node(GRAM_OPERATOR_OR);
+	// ast_add_child(or, and_2);
+	// ast_add_child(or, c5);
+	
+	t_ast *root = ast_new_node(GRAM_COMPLETE_COMMAND);
+	ast_add_child(root, p2);
+	// ast_add_child(root, or);
+	return root;
 }
 
-//? echo "echo", "$USER", "$SHELL", "'$PWD'"
-t_ast *ft_get_ast5(void)
+// echo "echo", "$USER", "$SHELL", "'$PWD'"
+t_ast	*ft_get_ast5(void)
 {
-    t_ast *root = ast_new_node(GRAM_COMPLETE_COMMAND);
-    
-    // 1) Simple command: cat
-    t_ast *cmd = ast_new_node(GRAM_SIMPLE_COMMAND);
-    cmd->data.args = ast_create_args(4, "echo", "$USER", "$SHELL", "'$PWD'");
+	t_ast *root = ast_new_node(GRAM_COMPLETE_COMMAND);
 
-    ast_add_child(root, cmd);
-    return root;
+	t_ast *cmd = ast_new_node(GRAM_SIMPLE_COMMAND);
+	cmd->data.args = ast_create_args("echo echo $USER $SHELL '$PWD'");
+
+	ast_add_child(root, cmd);
+	return root;
 }
 
 t_ast	*ft_get_ast_example(int n)
