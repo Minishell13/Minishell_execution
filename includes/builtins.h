a37abec3cc263267e653dc93d903cd07e4545572
Abdellah-Nsila <dawndawn074@gmail.com>
2025-05-29 16:17:18 +0100

    Update builtins header file

--- includes/builtins.h
+++ includes/builtins.h
@@ -5,17 +5,20 @@
 
 int		valid_key(char *arg);
 char	*extract_key(char *arg, int end);
-t_bool	check_input(char **args);
+t_bool	check_echo(char *arg);
+t_bool	no_options(char *arg);
+t_bool	no_args(char **args);
+int		builtins_error(char *cmd, char *arg, char *msg);
 
-t_error	exec_env(t_ast *node);
-t_error	exec_export(t_ast *node);
-t_error	exec_unset(t_ast *node);
-t_error exec_pwd(t_ast *node);
-t_error	exec_cd(t_ast *node);
-t_error	exec_echo(t_ast *node);
-t_error	exec_exit(t_ast *root);
+int		exec_env(t_ast *node);
+int		exec_export(t_ast *node);
+int		exec_unset(t_ast *node);
+int		exec_pwd(t_ast *node);
+int		exec_cd(t_ast *node);
+int		exec_echo(t_ast *node);
+int		exec_exit(t_ast *root, t_ast *node);
 
-t_bool  is_builtins(t_ast *node);
-t_error	exec_builtins(t_ast *root, t_ast *node);
+t_bool	is_builtins(t_ast *node);
+int		exec_builtins(t_ast *root, t_ast *node);
 
 #endif
